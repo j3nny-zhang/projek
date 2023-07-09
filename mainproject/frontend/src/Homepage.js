17b7373061a605584c63477650d8d0fda110b33a
Ryan Sun <ryiijcsun@gmail.com>
2023-07-09 15:58:55 -0400

    initial frontend design

---
+++ mainproject/frontend/src/Homepage.js
@@ -0,0 +1,9 @@
+const Homepage = () => {
+    return (
+        <div className="home">
+            <h2>Homepage :3</h2>
+        </div>
+    );
+}
+
+export default Homepage;
---
+++ mainproject/frontend/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Acountability Tracker</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Taskpage</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
