9f85e4bcf9a88b6f8ee5c47316c9d07cb40aea8f
Ryan Sun <ryiijcsun@gmail.com>
2023-08-08 11:39:25 -0400

    Created basic React layout

--- mainproject/frontend/src/App.js
+++ mainproject/frontend/src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
 import Navbar from './Navbar';
 import Homepage from './Homepage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreateTask from './CreateTask';
+import TaskDetails from './TaskDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <Homepage></Homepage>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="flex-container">
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Homepage></Homepage>}></Route>
+              <Route path="/createTask" element={<CreateTask></CreateTask>}></Route>
+              <Route path="/tasks/:id" element={<TaskDetails></TaskDetails>}></Route>
+              <Route path="*" element={<NotFound></NotFound>}></Route>
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- mainproject/frontend/src/Navbar.js
+++ mainproject/frontend/src/Navbar.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Acountability Tracker</h1>
+            <h1> PROJEK: <span style={{color: "#9BBBA2"}}>Acountability Tracker</span></h1>
+
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Taskpage</a>
+                <Link to="/">Homepage</Link>
+                <Link to="/createTask" style={{
+                    color: "white", 
+                    backgroundColor: '#618264',
+                    borderRadius: '8px'
+                }}>Add Task</Link>
             </div>
+
         </nav>
     );
 }
